3bb1df49c325461082875e5dc20bad301ece91f0
John Scharber <john@pavedroad.io>
2019-09-23 13:30:41 -0700

    Makefile - Added commands for starting, stopping, and getting status   of microk8s cluster. Stopping cluster also delete skaffold   deployments - deploy: now starts microk8s if it is not running - check: fixed typo **line** to check - misc: removed unused commands; exec, .... - Integrate go vet - Integrate gosec README.md - Initial commit, explains roadctl, make usage - Explain git usage - Explain integrated lint applications - Document versioning variables dev/... - kube-config.sh: sets users $HOME/.kube/config to current   microk8s instances - microk8sStart.sh: Starts microk8s if not running - microk8sStop.sh: Stops microk8s if running - microk8sStatus.sh: Prints the status of local k8s cluster templateMain.go: Added version option templateModel.go: Removed unused response that was   causing go-swagger to fail Closes: issue#10, https://github.com/pavedroad-io/roadctl/issues/10 Closes: issue#4, https://github.com/pavedroad-io/roadctl/issues/4

---
+++ microservices/ga/datamgr/dev/microk8sStatus.sh
@@ -0,0 +1,12 @@
+{{define "microk8sStatus.sh"}}
+#!/bin/bash
+
+STATUS=`microk8s.status | sed  's/.*\(\bnot running\b\).*/\1/'`
+
+if [ "$STATUS" = "not running" ]
+then
+	echo "down"
+else
+	echo "up"
+fi
+{{end}}
