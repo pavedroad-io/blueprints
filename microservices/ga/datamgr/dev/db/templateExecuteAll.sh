d3cd23631abe24a47299a19fa9c271058ee03755
John Scharber <john@pavedroad.io>
2019-12-02 10:21:29 -0800

    K8sfixes (#8)
    * Makefile
    - Added commands for starting, stoppin, and getting status
      of microk8s cluster. Stopping cluster also delete skaffold
      deployments
    - deploy: now starts microk8s if it is not running
    - check: fixed typo **line** to check
    - misc: removed unused commands; exec, ....
    README.md
    - Initial commit, explains roadctl, make usage
    dev/...
    - kube-config.sh: sets users $HOME/.kube/config to current
      microk8s instances
    - microk8sStart.sh: Starts microk8s if not running
    - microk8sStop.sh: Stops microk8s if running
    - microk8sStatus.sh: Prints the status of local k8s cluster
    templateMain.go: Added version option
    templateModel.go: Removed unused response that was
      causing go-swagger to fail
    Closes: issue#10, https://github.com/pavedroad-io/roadctl/issues/10
    Closes: issue#4, https://github.com/pavedroad-io/roadctl/issues/4
    
    * Makefile
    - Added commands for starting, stopping, and getting status
      of microk8s cluster. Stopping cluster also delete skaffold
      deployments
    - deploy: now starts microk8s if it is not running
    - check: fixed typo **line** to check
    - misc: removed unused commands; exec, ....
    - Integrate go vet
    - Integrate gosec
    README.md
    - Initial commit, explains roadctl, make usage
    - Explain git usage
    - Explain integrated lint applications
    - Document versioning variables
    dev/...
    - kube-config.sh: sets users $HOME/.kube/config to current
      microk8s instances
    - microk8sStart.sh: Starts microk8s if not running
    - microk8sStop.sh: Stops microk8s if running
    - microk8sStatus.sh: Prints the status of local k8s cluster
    templateMain.go: Added version option
    templateModel.go: Removed unused response that was
      causing go-swagger to fail
    Closes: issue#10, https://github.com/pavedroad-io/roadctl/issues/10
    Closes: issue#4, https://github.com/pavedroad-io/roadctl/issues/4
    
    * Makefile: cleanup remove unneeded quotes
    datmgr.txt: Add documentation about the defition file attributes for
    this micro service
    man/kubernetes: templates for generating k8s manifests required because
    of bug in kompose
    
    * Switch case in tests to use JSON field names instead of struct names
    
    * WIP#1
    - fix paths; NameExported -> Name code and test files
    - mkdir -p app/logs to aviod startup errors
    - add dev/busybox-diag.yaml
    - add getk8sip.sh to return IP address for current microk8s cluster
    - add getNodePort to return port for deployed service
    - modify testGetList, testDelete, testGet, testPut, testPost
    -- consitently set UUID when required
    -- -k or -k8s options support testing with local cluster
    - make testAll useful
    - manifest changes
    -- remove local configuration from skaffold.yaml
    -- modify template-deployment.yaml to use env overriding
    -- modify template-service.yaml now exposes service as NodePort
    
    * WIP#2
    - fix paths; NameExported -> Name code and test files
    - mkdir -p app/logs to aviod startup errors
    - add dev/busybox-diag.yaml
    - add getk8sip.sh to return IP address for current microk8s cluster
    - add getNodePort to return port for deployed service
    - modify testGetList, testDelete, testGet, testPut, testPost
    -- consitently set UUID when required
    -- -k or -k8s options support testing with local cluster
    - make testAll useful
    - manifest changes
    -- remove local configuration from skaffold.yaml
    -- modify template-deployment.yaml to use env overriding
    -- modify template-service.yaml now exposes service as NodePort
    
    * Support for kubernetes init containers:
    - Create db if not found
    
    * Add init container that waits for DB to be live
    
    * Change template to pull pavedroad cockroachdb-client image
    Remove duplicate Init container in deployment manifest
    Add closing "}" in function

--- microservices/ga/datamgr/dev/db/templateExecuteAll.sh
+++ microservices/ga/datamgr/dev/db/templateExecuteAll.sh
@@ -1,24 +1,75 @@
 {{define "templateExecuteAll.sh"}}
 #!/bin/bash
 
-CMD=`which cockroach`" sql"
+SQL="cockroach sql"
 PORT="26257"
-IP="127.0.0.1"
+HOST="127.0.0.1"
 USER="root"
+CMD=`echo $SQL "--insecure" --host=$HOST:$PORT`
 
-CMD=`echo $CMD "--insecure" --host=$IP:$PORT`
+buildCommand()
+{
+  CMD=`echo $SQL "--insecure" --host=$HOST:$PORT`
+}
 
-echo "$CMD"
+all()
+{
 
-#1 Create kevlarAdmin if it doesn not already exists
-$CMD < {{.Organization}}Admin.sql
+  echo "========================================"
+  echo " Initializing tables"
+  start=`date`
+  echo " Starting at : $start"
+  echo " Using: $CMD"
+  echo ""
 
-#2 Create kevlarWeb db
-$CMD < {{.Organization}}.sql
+# 1 Create {{.Organization}}Admin if it doesn not already exists
+  $CMD < {{.Organization}}Admin.sql
 
-#3 Create kevlarAdmin all on kevlarWeb db
-$CMD < {{.Organization}}GrantAdmin.sql
+# 2 Create {{.Organization}}Web db
+  $CMD < {{.Organization}}.sql
 
-#4 Create prTokenTable 
-$CMD < {{.Name}}CreateTable.sql
+# 3 Create A{{.Organization}}dmin all on kevlarWeb db
+  $CMD < {{.Organization}}GrantAdmin.sql
+
+# 4 Create {{.Organization}}Table 
+  $CMD < {{.Name}}CreateTable.sql
+}
+
+usage()
+{
+  echo "usage: {{.Organization}}ExecuteAll.sh -k |--k8s"
+  echo "    Created database and users as needed"
+  echo "    -k locates and posts to local k8s cluster"
+  echo "    it will default to $host on port $port"
+}
+
+## Main
+while [ "$1" != "" ]; do
+  case $1 in
+    -k | --k8s ) shift
+
+      if [ -z "$COCKROACH_HOST" ]
+      then
+        echo "COCKROACH_HOST must be set"
+        exit
+      else
+        HOST=$COCKROACH_HOST
+      fi
+
+      if [ -v "$COCKROACH_PORT" ]
+      then
+        PORT=$COCKROACH_PORT
+      fi
+      buildCommand
+      ;;
+  -h | --help ) usage
+    exit
+    ;;
+  * ) shift
+    ;;
+  esac
+done
+
+# call all
+all
 {{end}}
--- microservices/ga/datamgr/dev/testGetList.sh
+++ microservices/ga/datamgr/dev/testGetList.sh
@@ -1,8 +1,36 @@
 {{define "testGetList.sh"}}
 #!/bin/bash
+host=127.0.0.1
+port=8081
+service="{{.Name}}"
 
-curl -H "Content-Type: application/json" \
-     -v http://localhost:8083/api/v1/namespace/pavedroad.io/{{.Name}}LIST | jq '.'
+get()
+{
+  curl -v -H "Content-Type: application/json" -s http://$host:$port/api/v1/namespace/pavedroad.io/$service"LIST"/ | jq '.'
+}
+
+usage()
+{
+  echo "usage: testGetList.sh -k |--k8s"
+  echo "    -k locates ands posts to local k8s cluster"
+  echo "    Otherwise, it will post to $host on port $port"
+}
+
+## Main
+while [ "$1" != "" ]; do
+  case $1 in
+    -k | --k8s ) shift
+      host="$(./getk8sip.sh)"
+      port="$(./getNodePort.sh $service)"
+      ;;
+  -h | --help ) usage
+    exit
+    ;;
+  * ) shift
+    ;;
+  esac
+done
+
+# Call get
+get
 {{end}}
-
-
