f295aeb5e221c78778ff998f57211df7a1004c8d
John Scharber <john@pavedroad.io>
2020-01-12 12:43:00 -0800

    Add Rest end-points for Schedule Add select to ScheduleRun to allow it to be stopped

--- microservices/ga/workerPool/templateScheduler.go
+++ microservices/ga/workerPool/templateScheduler.go
@@ -7,23 +7,23 @@ import "os"
 type Scheduler interface {
 	// Data methods
 	// For schedulers
-	GetSchedule() (Scheduler, error)
-	UpdateSchedule() (Scheduler, error)
-	CreateSchedule() (Scheduler, error)
-	DeleteSchedule() (Scheduler, error)
+	GetSchedule() (httpStatusCode int, jsonBlob []byte, err error)
+	UpdateSchedule(jsonBlob []byte) (httpStatusCode int, jsonb []byte, err error)
+	CreateSchedule(jsonBlob []byte) (httpStatusCode int, jsonb []byte, err error)
+	DeleteSchedule() (httpStatusCode int, jsonb []byte, err error)
 
 	 // For jobs
-  GetScheduledJobs() ([]byte, error)
-  GetScheduleJob(UUID string) (httpStatusCode int, jsonBlob []byte, err error)
-  UpdateScheduleJob(jsonBlob []byte) (httpStatusCode int, jsonb []byte, err error)
-  CreateScheduleJob(jsonBlob []byte) (httpStatusCode int, jsonb []byte, err error)
-  DeleteScheduleJob(UUID string) (httpStatusCode int, jsonb []byte, err error)
+	GetScheduledJobs() ([]byte, error)
+	GetScheduleJob(UUID string) (httpStatusCode int, jsonBlob []byte, err error)
+	UpdateScheduleJob(jsonBlob []byte) (httpStatusCode int, jsonb []byte, err error)
+	CreateScheduleJob(jsonBlob []byte) (httpStatusCode int, jsonb []byte, err error)
+	DeleteScheduleJob(UUID string) (httpStatusCode int, jsonb []byte, err error)
 
 	// Execution methods
 	Init() error
 	SetChannels(chan Job, chan Result, chan bool, chan os.Signal)
 	Shutdown() error
-  //Status()
+	//Status()
 
 	// Status methods
 	Metrics() []byte
