e50dbdcdccafa7945dc93da31ec36406006492b6
John Scharber <john@pavedroad.io>
2021-09-27 16:11:49 -1000

    Fix spelling of staging Fix adding definitions in Makefile Remove unneeded resources in kustomize staging configurations

--- microservices/ga/datamgr/dev/make-stagging-repo.sh
+++
@@ -1,5 +0,0 @@
-{{define "dev/make-stagging-repo.sh"}}
-#!/bin/bash
-aws ecr create-repository --repository-name io.pavedroad.stagging/{{.Name}}initdb --region us-west-1 > ../docs/staggging-repo-{{.Name}}initdb.json
-aws ecr create-repository --repository-name io.pavedroad.stagging/{{.Name}} --region us-west-1 > ../docs/staggging-repo-{{.Name}}.json
-{{end}}
